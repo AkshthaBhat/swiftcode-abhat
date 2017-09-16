6417ab144ad0556f9543d2d3b8ab8fe07b04ac60
AkshthaBhat <akshathabhat19@gmail.com>
2017-09-16 10:28:46 +0000

    working bot

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,33 +1,21 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
-app.controller('chatController', function ($scope, $sce){
-    $scope.messages = [
-    {
-      sender:"BOT",
-      text:"Hey how can I help you?",
-      time:"19:19"
-    },
-    {
-      sender: "USER",
-      text:"Who are you?",
-      time:"19:19"
-    },
-    {
-      sender: "BOT",
-      text: "Moi is Bot the Bot",
-      time: "19:20"
-    }];
+app.controller('chatController', function($scope, $sce){
+    $scope.messages = [];
 
-    $scope.sendMessage = function() {
-    exampleSocket.send($scope.userMessage);
-    $scope.userMessage = "";
-    };
+  
     var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
     exampleSocket.onmessage  =   function  (event) {
-           var jsonData = JSON.parse(event.data);
-           jsonData.time = new Date().toLocateTimeString;
-           console.log(jsonData);
-       };
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date().toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply(); 
+        console.log(jsonData);    
+    };
+    $scope.sendMessage = function() {
+        exampleSocket.send($scope.userMessage);
+        $scope.userMessage = "";
+    };
 
- $scope.trust= $sce.trustAsHtml;
+    $scope.trust = $sce.trustAsHtml;
 });
